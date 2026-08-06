2e9ea142ebe63fb4aeb04703828b46ba5c2506f3
jhyeonjung97 <hyeonjung@postech.ac.kr>
2026-08-05 17:14:39 -0700

    .

--- examples/4_MnO2_100/command-simple.sh
+++ examples/4_MnO2_100/command-simple.sh
@@ -6,16 +6,17 @@ python ../../HybridPourbaix.py --hybrid --HER --OER \
 --Umin -0.5 --Umax 2.0 --suffix Mn --colors-bulk white gainsboro silver gray dimgray silver gainsboro white
 
 # hybrid
-mkdir -p OH/
-mv OH/OHBridge_OHTop.json .
 python ../../HybridPourbaix.py --OER --hybrid --no-bulk --Umin -0.5 --Umax 2.0 \
 --colors-bulk white whitesmoke lightgray darkgray gray darkgray whitesmoke gold white lemonchiffon \
 --colors-2d dodgerblue lightskyblue --suffix small
 
-mv OH/OHBridge_OHTop.json .
 python ../../HybridPourbaix.py --hybrid --no-bulk --Umin -0.5 --Umax 2.0 \
 --colors-bulk white whitesmoke lightgray darkgray gray darkgray whitesmoke gold white lemonchiffon \
 --colors-2d dodgerblue lightskyblue --figx 6 --figy 6 --suffix large
+
+python ../../HybridPourbaix.py --hybrid --no-bulk --Umin 0.0 --Umax 2.0 --figx 2.5 --figy 2.5 \
+--colors-bulk white whitesmoke lightgray darkgray lightgray gold white \
+--colors-2d dodgerblue lightskyblue --suffix toc
 
 # hybrid without MnO4-
 mv OH/OHBridge_OHTop.json .
@@ -29,3 +30,4 @@ mv OHBridge_OHTop.json OH/
 python ../../HybridPourbaix.py --hybrid --no-bulk --Umin -0.5 --Umax 2.0 \
 --colors-bulk white whitesmoke lightgray darkgray gray darkgray whitesmoke lightgray white lemonchiffon \
 --colors-2d dodgerblue lightskyblue --figx 6 --figy 6 --suffix OH
+mv OH/OHBridge_OHTop.json .
